3deaae0c241bbbb1e621da8ff0ed580bd508671c
John Jairo Quintero Pardo <johnki@MacBook-Air-de-John.local>
2020-04-30 08:51:28 -0500

    first commit

---
+++ WebAppAlbunesJJQP/Models/album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppAlbunesJJQP.Models
+{
+    public class album
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+}
---
+++ WebAppAlbunesJJQP/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppAlbunesJJQP.Startup))]
+namespace WebAppAlbunesJJQP
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
